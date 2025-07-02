3de61e57ad6ced4601a3dd29369133f3476ee3fe
bảo đỗ <baodo432@gmail.com>
2025-07-02 20:34:59 +0700

    Add project files.

---
+++ API/Program.cs
@@ -0,0 +1,70 @@
+﻿using API.IRepository;
+using API.IRepository.Repository;
+using API.Repository;
+using Data.IRepository;
+using Data.Models;
+using Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Đăng ký DbContext (chỉ 1 lần duy nhất)
+builder.Services.AddDbContext<DbContextApp>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
+
+
+
+// Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Đăng ký Repository
+builder.Services.AddScoped<IAnhRepository, AnhRepository>();
+builder.Services.AddScoped<IChatLieuRepository, ChatLieuRepository>();
+builder.Services.AddScoped<IDeGiayRepository, DeGiayRepository>();
+builder.Services.AddScoped<IGiamGiaRepository, GiamGiaRepository>();
+builder.Services.AddScoped<IGiayRepository, GiayRepository>();
+builder.Services.AddScoped<IThongBaoRepository, ThongBaoRepository>();
+builder.Services.AddScoped<IDiaChiKhachHangRepository, DiaChiKhachHangRepository>();
+builder.Services.AddScoped<IGiayChiTietRepository, GiayChiTietRepository>();
+builder.Services.AddScoped<IGioHangRepository, GioHangRepository>();
+builder.Services.AddScoped<IGioHangChiTietRepository, GioHangChiTietRepository>();
+builder.Services.AddScoped<IHoaDonRepo, HoaDonRepo>();
+builder.Services.AddScoped<IMauSacRepository, MauSacRepository>();
+builder.Services.AddScoped<IKichCoRepository, KichCoRepository>();
+builder.Services.AddScoped<INhanVienRepository, NhanVienRepository>();
+builder.Services.AddScoped<IThuongHieuRepository, ThuongHieuRepository>();
+builder.Services.AddScoped<IVoucherRepo, VoucherRepo>();
+builder.Services.AddScoped<IKieuDangRepository, KieuDangRepository>();
+builder.Services.AddScoped<ITheLoaiGiayRepository, TheLoaiGiayRepository>();
+builder.Services.AddScoped<IChiTietHoaDonRepository, ChiTietHoaDonRepository>();
+builder.Services.AddScoped<ITaiKhoanRepository, TaiKhoanRepository>();
+builder.Services.AddScoped<IChucVuRepository, ChucVuRepository>();
+builder.Services.AddScoped<IReturnRepository, ReturnRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
